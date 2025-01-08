7af3ab262dc4966028b998af1acd7428a63bd638
Jamo4405 <jamysongouveros4405@gmail.com>
2025-01-08 18:33:39 +1100

    updated logo on loading screen

--- src/app/components/LoadingScreen.tsx
+++ src/app/components/LoadingScreen.tsx
@@ -8,18 +8,18 @@ export default function LoadingScreen({ children }: { children: React.ReactNode 
   const loadingScreenRef = useRef<HTMLDivElement | null>(null);
 
   const handleTransitionEnd = () => {
-    // After fade-out completes, hide the loading screen
+
     setIsVisible(false);
   };
 
   useEffect(() => {
     if (isFadingOut && loadingScreenRef.current) {
-      // Listen for the end of the fade-out transition
+
       loadingScreenRef.current.addEventListener("transitionend", handleTransitionEnd);
     }
 
     return () => {
-      // Cleanup the event listener
+
       if (loadingScreenRef.current) {
         loadingScreenRef.current.removeEventListener("transitionend", handleTransitionEnd);
       }
@@ -27,13 +27,13 @@ export default function LoadingScreen({ children }: { children: React.ReactNode 
   }, [isFadingOut]);
 
   useEffect(() => {
-    // Simulate a loading process, then fade-out after images have loaded or after a timeout
+
     const timeoutId = setTimeout(() => {
       setIsFadingOut(true);
-    }, 1000); // You can adjust this timeout to match your needs
+    }, 1000);
 
     return () => {
-      clearTimeout(timeoutId); // Cleanup the timeout on unmount
+      clearTimeout(timeoutId);
     };
   }, []);
 
@@ -44,7 +44,8 @@ export default function LoadingScreen({ children }: { children: React.ReactNode 
           ref={loadingScreenRef}
           className={`loading-screen ${isFadingOut ? "fade-out" : ""}`}
         >
-          <p className="loading-text">VERITAS</p>
+          <img src={"/blogo.svg"} alt={"logo"} width={40} height={40}/>
+          <p className="loading-text pl-1.5">VERITAS <sup>®</sup></p>
         </div>
       )}
       <div className={isVisible ? "content-hidden" : ""}>
